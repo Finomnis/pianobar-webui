9aa9996925cadafe5af852c80d2c14139191b493
Finomnis <finomnis@gmail.com>
2021-02-20 17:02:49 +0100

    Add run script

---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -eu
+
+SCRIPTPATH=$( cd "$(dirname "$(readlink -f "$0")")"; pwd -P )
+
+# Update venv
+"${SCRIPTPATH}/create_venv.sh"
+
+# Activate venv
+source "${SCRIPTPATH}/.venv/bin/activate"
+
+# Start processes
+pianobar-websocket-simulate-events &
+sleep 20 &
+
+# Exit on failure
+wait -n
+echo "One of the background processes exited with '$?'."
+
+echo "Killing other background processes ..."
+BACKGROUND_JOBS=$(jobs -p)
+if [ -n "${BACKGROUND_JOBS}" ]; then
+    echo "Killing ${BACKGROUND_JOBS} ..."
+    kill ${BACKGROUND_JOBS}
+fi
+echo "Exiting program."
