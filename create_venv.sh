ef7702ed81f003b42d2a15dc72cc3406a7150341
Finomnis <finomnis@gmail.com>
2021-02-20 16:41:24 +0100

    Add virtual environment creation script, adjust folder names

---
+++ create_venv.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# This script creates or updates the virtual environment named '.venv'
+# in the repository root.
+# To install a new package in the virtual environment, modify the
+# 'requirements.txt' in the repository root and run this script again.
+
+set -e -u
+SCRIPTPATH=$( cd "$(dirname "$(readlink -f "$0")")"; pwd -P )
+REPOROOT=$( cd "$SCRIPTPATH/."; pwd -P )
+
+venvpath="$REPOROOT/.venv"
+
+# Create environment if it doesn't exist yet, or update it otherwise:
+if [[ ! -d "$venvpath" ]]; then
+    python3 -m venv "$venvpath"
+else
+    python3 -m venv --upgrade "$venvpath"
+fi
+
+# Activate environment
+source "$venvpath/bin/activate"
+
+# Install newer pip and requirements
+pip3 install --force-reinstall pip==21.0.1
+pip3 install --force-reinstall wheel==0.36.2
+
+(cd "$REPOROOT" && pip3 install -r ./requirements.txt)
+
+echo ""
+echo "Setting up environment finished!"
+echo "Activate environment:"
+echo "source ${venvpath}/bin/activate"
