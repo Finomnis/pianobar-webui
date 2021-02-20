9fcc8d3c72b4ba83f326670c0e4750abbd5573c8
Finomnis <finomnis@gmail.com>
2021-02-20 17:17:32 +0100

    More work on socket

---
+++ pianobar-websocket/pianobarsocket/websocket/event_receiver.py
@@ -0,0 +1,3 @@
+class EventReceiver:
+    def __init__(self):
+        pass
--- pianobar-websocket/pianobarsocket/websocket/websocket.py
+++ pianobar-websocket/pianobarsocket/websocket/websocket.py
@@ -1,8 +1,28 @@
 import asyncio
+import websockets
+import logging
+
+from pianobarsocket import settings
+from event_receiver import EventReceiver
+
+_logger = logging.getLogger(__name__)
+
+
+#async def websocket_connection(websocket, path):
+#    _logger.info(f"Connected: {path}")
+#    msg = await websocket.recv()
+#    print(msg)
+
+
+async def websocket_main():
+#    await websocket.serve(websocket_handler, port=settings.WEBSOCKET_PORT)
+
 
 
 async def main():
-    print("Websocket main!")
+    eventReceiver = EventReceiver()
+
+    _logger.info("Websocket main!")
 
 
 def run():
