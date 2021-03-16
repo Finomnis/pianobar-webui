bdc086c341a7152dabc676740541566c3fff0653
Martin Stumpf <finomnis@gmail.com>
2021-03-16 01:23:30 +0100

    Add debug infos to pianobar actions

--- pianobar_webserver/src/bin/pianobar_webserver/pianobar_controller/actions.rs
+++ pianobar_webserver/src/bin/pianobar_webserver/pianobar_controller/actions.rs
@@ -41,26 +41,32 @@ impl PianobarActions {
     }
 
     pub async fn change_station(&self, station_id: usize) -> Result<()> {
+        log::info!("Changing station to #{} ...", station_id);
         self.simple_command(&format!("s{}\n", station_id)).await
     }
 
     pub async fn pause(&self) -> Result<()> {
+        log::info!("Pausing ...");
         self.simple_command("S").await
     }
 
     pub async fn resume(&self) -> Result<()> {
+        log::info!("Resuming ...");
         self.simple_command("P").await
     }
 
     pub async fn toggle_pause(&self) -> Result<()> {
+        log::info!("Toggling pause ...");
         self.simple_command("p").await
     }
 
     pub async fn skip(&self) -> Result<()> {
+        log::info!("Skipping ...");
         self.simple_command("n").await
     }
 
     pub async fn explain(&self) -> Result<String> {
+        log::info!("Explaining ...");
         let (mut _receiver, mut actor) = self.connect().await;
 
         actor.write(&with_reset("e")).await?;
