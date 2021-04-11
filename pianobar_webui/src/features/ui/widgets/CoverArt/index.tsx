fdf5f00ec802fe21453961e7d141367e663c094d
Martin Stumpf <finomnis@gmail.com>
2021-04-11 14:31:55 +0200

    Add automatic image fitting to CoverArt widget

--- pianobar_webui/src/features/ui/widgets/CoverArt/index.tsx
+++ pianobar_webui/src/features/ui/widgets/CoverArt/index.tsx
@@ -1,53 +1,59 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectPianobarCoverArt } from "../../../pianobar/store/selector";
+import AutoSizer from "react-virtualized-auto-sizer";
 import note from "./musical-note.svg";
+import { Box } from "@material-ui/core";
 
-type CoverArtProps = {
-    width: string;
-    height: string;
-};
 
-const CoverArt = (props: CoverArtProps) => {
+const CoverArt = () => {
     let coverArtUrl = useSelector(selectPianobarCoverArt);
     return (
-        <div
-            style={{
-                display: "flex",
-                background: "black",
-                width: props.width,
-                height: props.height,
+        <AutoSizer>
+            {({ height, width }) => {
+                const length = Math.min(width, height);
+                return (
+                    <Box
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                        width={width}
+                        height={height}
+                        overflow="hidden"
+                    >
+                        { coverArtUrl === "" ? (
+                            <div
+                                style={{
+                                    width: length,
+                                    height: length,
+                                    display: "flex",
+                                    background: "#d3d3d3",
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                }}
+                            >
+                                <img
+                                    src={note}
+                                    alt="coverArtPlaceholder"
+                                    width="50%"
+                                    height="50%"
+                                    key="coverArtFallback"
+                                />
+                            </div>
+                        ) : (
+                            <img
+                                src={coverArtUrl}
+                                alt="coverArt"
+                                width={length}
+                                height={length}
+                                key="coverArt"
+                            />
+                        )
+                        }
+                    </Box>
+                );
             }}
-        >
-            {coverArtUrl === "" ? (
-                <div
-                    style={{
-                        width: "100%",
-                        height: "100%",
-                        display: "flex",
-                        background: "#d3d3d3",
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}
-                >
-                    <img
-                        src={note}
-                        alt="coverArtPlaceholder"
-                        width="50%"
-                        height="50%"
-                        key="coverArtFallback"
-                    />
-                </div>
-            ) : (
-                <img
-                    src={coverArtUrl}
-                    alt="coverArt"
-                    width="100%"
-                    height="100%"
-                    key="coverArt"
-                />
-            )}
-        </div>
+        </AutoSizer >
     );
 };
 export default React.memo(CoverArt);
